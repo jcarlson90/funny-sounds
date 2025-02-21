e92555b12d3717778b7ac9df5b380e977f27f4bc
jcarlson <jake.carlson90@gmail.com>
2025-02-21 14:02:15 -0800

    yup

---
+++ src/App.js
@@ -0,0 +1,37 @@
+// fart three is bass boosted
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <app>
+    
+    <div className="fart-btns">
+    <header className="title">FART BOX</header>
+      <div className="one-two">
+        <button className="squeaky">1. Squeaky</button>
+        <button className="wet">2. Wet</button>
+      </div>
+
+      <div className="three-four">
+        <button className="loud">3. Loud</button>
+        <button className="quick">4. Quick</button>
+      </div>
+
+      <div className="five-six">
+        <button className="long">5. Long</button>
+        <button className="meme-1">6. Meme 1</button>
+      </div>
+
+      <div className="seven-eight">
+        <button className="meme-2">7. Meme 2</button>
+        <button className="meme-3">8. Meme 3</button>
+      </div>
+    </div>
+    </app>
+
+  );
+}
+
+export default App;
+
