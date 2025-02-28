edfedd6abeae2211816cbfcfaa80c557f23dbbcb
jcarlson <jake.carlson90@gmail.com>
2025-02-28 13:43:15 -0800

    more

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 // fart three is bass boosted
 import React from "react";
 import "./App.css";
+import memeOneSound from './sounds/meme-one.wav'
+import memeTwoSound from './sounds/meme-two.wav'
+import quickSound from './sounds/quick-fart.wav'
 
 function App() {
+  const playSound = (soundFile) => {
+    const audio = new Audio(soundFile);
+    audio.play();
+  };
+
   return (
     <app>
     
@@ -15,16 +23,16 @@ function App() {
 
       <div className="three-four">
         <button className="loud">3. Loud</button>
-        <button className="quick">4. Quick</button>
+        <button className="quick" onClick={() => playSound(quickSound)}>4. Quick</button>
       </div>
 
       <div className="five-six">
         <button className="long">5. Long</button>
-        <button className="meme-1">6. Meme 1</button>
+        <button className="meme-1" onClick={() => playSound(memeOneSound)}>6. Meme 1</button>
       </div>
 
       <div className="seven-eight">
-        <button className="meme-2">7. Meme 2</button>
+        <button className="meme-2" onClick={() => playSound(memeTwoSound)}>7. Meme 2</button>
         <button className="meme-3">8. Meme 3</button>
       </div>
     </div>
